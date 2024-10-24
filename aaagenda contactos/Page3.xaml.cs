9543aaf3558cecea1cbba87bb447446162897d7c
Miguel <miguelisto09@gmail.com>
2024-10-24 14:47:37 -0400

    Errores Corregidos

--- aaagenda contactos/Page3.xaml.cs
+++ aaagenda contactos/Page3.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Contactos
 {
@@ -24,16 +15,36 @@ namespace Contactos
         {
             InitializeComponent();
 
+            // Registrar el convertidor como recurso de la página
+            this.Resources.Add("BoolToVis", new BoolToVisConverter());
         }
+
         private void CerrarVentana_Click(object sender, RoutedEventArgs e)
         {
             Window.GetWindow(this)?.Close();
         }
+
         private void MinimizarVentana_Click(object sender, RoutedEventArgs e)
         {
+            Window.GetWindow(this)?.Close();
+        }
+    }
+
+    // Convertidor que convierte de bool a Visibility
+    public class BoolToVisConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
             {
-                Window.GetWindow(this)?.Close();
+                return boolValue ? Visibility.Visible : Visibility.Collapsed;
             }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
