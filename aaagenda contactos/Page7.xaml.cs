94ef312b730ece0b4dd507ea95c194855f867285
Miguel <miguelisto09@gmail.com>
2024-11-28 08:45:21 -0400

    Tipo de telefono agregado

--- aaagenda contactos/Page7.xaml.cs
+++ aaagenda contactos/Page7.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -29,5 +30,35 @@ namespace aaagenda_contactos
         {
 
         }
+        private void CerrarVentana_Click(object sender, RoutedEventArgs e)
+        {
+            var ventana = Window.GetWindow(this) as MainWindow;
+            if (ventana != null)
+            {
+
+                var frame = ventana.FindName("Frame6") as Frame;
+                if (frame != null)
+                {
+
+                    ventana.ToggleMenu(sender, e);
+
+
+                    var fadeOutAnimation = new DoubleAnimation
+                    {
+                        To = 0,
+                        Duration = TimeSpan.FromMilliseconds(300),
+                        EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
+                    };
+
+                    fadeOutAnimation.Completed += (s, args) =>
+                    {
+                        frame.Visibility = Visibility.Collapsed;
+                    };
+
+                    frame.BeginAnimation(UIElement.OpacityProperty, fadeOutAnimation);
+                }
+            }
+        }
+
     }
 }
